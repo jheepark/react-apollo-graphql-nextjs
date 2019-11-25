5a09ac68853c4d1d8047f1effc6cdab33bcc3e56
jheepark <jhee.park.work@gmail.com>
2019-11-25 14:08:02 +1100

    chore: start from start

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,18 +1,1 @@
-require('dotenv').config({path: 'variables.env'});
-const createServer = require('createServer');
-const db = require('./db');
-
-const server = createServer();
-
-// Todo Use express middleware to handle cookies (JWT)
-// Todo Use express middleware to populate current user
-
-server.start({
-  // we only want to endpoint to be visited from our approved urls
-  cors: {
-    credentials: true,
-    origin: process.env.FRONTEND_URL,
-  },
-}, deets => {
-  console.log(`Server is now running on port https:/localhost:${deets.port}`)
-});
+// let's go!
