39c3310822715cf98242aba8248f9ca9c9372bd8
jheepark <jhee.park.work@gmail.com>
2019-11-25 17:08:27 +1100

    style: add styled components

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,14 +1,28 @@
 import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 
-const Nav = () => (
-	<div>
-		<Link href="/">
-			<a>Home!</a>
+const navItems = [
+	{link:'/items', title:'Items'},
+	{ link: '/sell', title: 'Sell' },
+	{ link: '/signup', title: 'Signup' },
+	{ link: '/orders', title: 'Orders' },
+	{ link: '/account', title: 'My Account' },
+];
+
+const Nav = () => {
+
+	const links = navItems.map((item) => (
+		<Link href={item.link} key={item.title}>
+			<a>{item.title}</a>
 		</Link>
-		<Link href="/sell">
-			<a>Sell!</a>
-		</Link>
-	</div>
-)
+	));
+
+	return (
+		<NavStyles>
+			{links}
+		</NavStyles>
+
+	)
+};
 
 export default Nav;
