5d2bed1a4c799601cd995a155d91aa2ca1329e5f
jheepark <jhee.park.work@gmail.com>
2019-11-27 16:47:27 +1100

    chore: add updated of crud

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -2,6 +2,7 @@ const { forwardTo } = require('prisma-binding');
 
 const Query = {
 	items: forwardTo('db'),
+	item: forwardTo('db'),
 };
 // this allows us to use the exact same api on the server as well as on the client.
 
